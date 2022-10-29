261d8dee2c58ef14f4c3a103e1be8b99364e1fa1
Vinícius Hansen <vinicius.carlo.hansen@gmail.com>
2022-10-29 14:13:40 -0300

    feat: cria header da FDE

---
+++ FDE/arq.h
@@ -0,0 +1,34 @@
+typedef struct elem{
+    int valor;
+    int prioridade;
+    struct elem* prox;
+    struct elem* ant;
+}Elemento;
+
+typedef struct descritor{
+    int TamLista;
+    int TamInfo;
+    Elemento cauda;
+    Elemento frente;
+    Elemento ref_movel;
+}descF;
+
+descF* cria(int tamInfo);
+
+int insereNaFila(info *novo,struct descF *p);
+
+int tamanhoDaFila(struct descF *p);
+
+int reinicia(struct descF *p);
+
+descF* destroi(struct descF *p);
+
+int buscaNaCauda(info *reg, struct descF *p);
+
+int buscaNaFrente(info *reg, struct descF *p);
+
+int buscaNoRefMovelFrente(info *reg, struct descF *p);
+
+int retiraDaFila(info *reg, struct descF *p); int testaVazia(struct descF *p);
+
+int inverte(struct descF *p); struct descF * destroi(struct descF *p);
